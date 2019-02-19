9f30541be270778efedda605fb1a4c135fe9c405
juliogarcia0205 <43921695+juliogarcia0205@users.noreply.github.com>
2019-02-19 17:58:23 -0600

    Update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,76 +1,79 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <frc/Joystick.h>
-#include <frc/XboxController.h>
-#include <frc/Talon.h>
-#include <frc/Spark.h>
+
+#include <GripPipeline.h>
+#include <cameraserver/CameraServer.h>
 #include <ctre/Phoenix.h>
+#include <frc/Compressor.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/Joystick.h>
+#include <frc/RobotDrive.h>
 #include <frc/SampleRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
 #include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/RobotDrive.h>
-#include <cameraserver/CameraServer.h>
-#include <frc/Timer.h>
-#include <GripPipeline.h>
-#include <frc/Compressor.h>
 #include <frc/Solenoid.h>
-#include <frc/DoubleSolenoid.h>
+#include <frc/Spark.h>
+#include <frc/Talon.h>
+#include <frc/Timer.h>
+#include <frc/XboxController.h>
+
 #define SRC_VISIONMETHODS_H_
+
 using namespace frc;
 
-//andrew
-class Robot: public frc::SampleRobot {
-	//Left And Right Motor
+// andrew
+class Robot: public frc::SampleRobot
+{
+	// Motors
 	Talon left {0}, right {1}, elevTT {6}; 
 		
-	//Controller And Joysticks 
-	XboxController controller {0},ElevCont {1};
-	
+	// Controllers 
+	XboxController controller {0}, ElevCont {1};
 
-	//Solenoids
+	// Solenoids
 	DoubleSolenoid largeC {0, 1}, releaseC {2, 3};
-	//AUTO CHOOSER
+	
+	// AUTO CHOOSER
 	SendableChooser<std::string> chooser;
 	const std::string autoNameDefault = "Default";
-	const std::string autoLeft = "Left Auto";
-	const std::string autoCenter = "Center Auto";
-	const std::string autoRight = "Right Auto";
-	const std::string autoTesting = "Compressor ON";
-	const std::string kR = "kReverse";
-	const std::string kF= "kForward";
-	const std::string kO= "kOff";
+	const std::string autoLeft        = "Left Auto";
+	const std::string autoCenter      = "Center Auto";
+	const std::string autoRight       = "Right Auto";
+	const std::string autoTesting     = "Compressor ON";
+	const std::string kR              = "kReverse";
+	const std::string kF              = "kForward";
+	const std::string kO              = "kOff";
+	
+	// Compressor
 	Compressor *c = new Compressor(0);
-	bool enabled = c->Enabled();
+	bool enabled        = c->Enabled();
 	bool pressureSwitch = c->GetPressureSwitchValue();
-	double current = c->GetCompressorCurrent();
+	double current      = c->GetCompressorCurrent();
 
-
-	//Drive Train Gear Mod
+	// Control Variables
+	float forwardVelocity, backwardsVelocity, turnVelocity;
+	float elevFoward, elevBackward;
+	double netLeft, netRight;
+	double netFoward, netBackward;
+	
+	// Drive Train Gear Mod
 	int gear = 3;
 	double speedMod = 1;
 
-	//Elevator Gear Mod
+	// Elevator Gear Mod
 	int elevGear = 1;
 	double elevSpeed = 1;
-
-	//Position Vars
-	float xAxis = 0;
-	float yAxis = 0;
-	float zAxis = 0;
-
-
-	 
-
 
 public:
 
 	Robot() {
-		//Note SmartDashboard is not initialized here, wait until RobotInit to make SmartDashboard calls
+		// Note SmartDashboard is not initialized here,	\
+		   wait until RobotInit to make SmartDashboard calls
 	}
 
-	void RobotInit() {
-
+	void RobotInit()
+	{
 		chooser.AddDefault(autoNameDefault, autoNameDefault);
 		chooser.AddObject(autoLeft, autoLeft);
 		chooser.AddObject(autoCenter, autoCenter);
@@ -79,21 +82,22 @@ public:
 		chooser.AddObject(kF, kF);
 		chooser.AddObject(kR, kR);
 		chooser.AddObject(kO, kO);
+		
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
+
 		cs::UsbCamera camera1 = CameraServer::GetInstance()->StartAutomaticCapture();
 		cs::UsbCamera camera2 = CameraServer::GetInstance()->StartAutomaticCapture();
 		cs::CvSink GetVideo(const cs::VideoSource& camera);
 
 		largeC.Set(DoubleSolenoid::Value::kOff);
 		releaseC.Set(DoubleSolenoid::Value::kOff);
-
-
 	}
 
-	void Autonomous() {
+	void Autonomous()
+	{
 		auto autoSelected = chooser.GetSelected();
 		std::cout << "Auto selected: " << autoSelected << std::endl;
-
+		
 		if (autoSelected == autoLeft) {
 			std::cout << "Running Left Lane Autonomous" << std::endl;
 			right.Set(-0.5);
@@ -118,21 +122,18 @@ public:
 			c->Start();
 			Wait(60);
 			c->Stop();
-
 		}
-		else if (autoSelected == kR){
+		else if (autoSelected == kR) {
 			std::cout << "kReverse" << std::endl;
 			largeC.Set(DoubleSolenoid::Value::kReverse);
 			Wait(1);
 			largeC.Set(DoubleSolenoid::Value::kOff);
-
 		}
-		else if (autoSelected == kF){
+		else if (autoSelected == kF) {
 			std::cout << "kForward" << std::endl;
 			largeC.Set(DoubleSolenoid::Value::kForward);
 			Wait(1);
 			largeC.Set(DoubleSolenoid::Value::kOff);
-
 		}
 		else if (autoSelected == kO) {
 			std::cout << "kOff" << std::endl;
@@ -143,13 +144,16 @@ public:
 		}
 	}
 
-	//Code run when operating
-
+	// Code run when operating
 	void OperatorControl() override {
-		while (IsOperatorControl() && IsEnabled()) {
-
-			//Gear selector
-
+		while (IsOperatorControl() && IsEnabled())
+		{
+			
+			/** -------- Motor Control ------------------ */
+			
+			/// Drive train
+			
+			// Gear select
 			if(controller.GetPOV() == 0) {
 				gear = 3;
 				speedMod = 1;
@@ -162,40 +166,24 @@ public:
 				gear = 1;
 				speedMod = 0.25;
 			}
+			
+			// Movement
+			forwardVelocity = controller.GetRawAxis(2);
+			backwardsVelocity = controller.GetRawAxis(3);
+			turnVelocity = controller.GetRawAxis(0);
 
-			//Output speed and gear to SmartDashboard
-
-			SmartDashboard::PutNumber("Speed Gear", gear);
-			//SmartDashboard::PutNumber("Speed", (left.GetSpeed() + (-right.GetSpeed()))/2);
-
-			//Movement variables
-
-			float forwardVelocity = controller.GetRawAxis(2);
-			float backwardsVelocity = controller.GetRawAxis(3);
-			float turnVelocity = controller.GetRawAxis(0);
-
-			float forwardVelocityLeft = forwardVelocity;
-			float backwardsVelocityLeft = backwardsVelocity;
-			float forwardVelocityRight = forwardVelocity;
-			float backwardsVelocityRight = backwardsVelocity;
-
-			double totalTurnVelocity = (turnVelocity * 1);// * 1.5;
-
-			double netLeft = ((forwardVelocityLeft - backwardsVelocityLeft) + totalTurnVelocity);
-			double netRight = ((backwardsVelocityRight - forwardVelocityRight) + totalTurnVelocity);
-
-			//Move main motors
+			netLeft = (forwardVelocity - backwardsVelocity) + turnVelocity;
+			netRight = (backwardsVelocity - forwardVelocity) + turnVelocity;
 
 			left.Set(netLeft * speedMod);
 			right.Set(netRight * speedMod);
 
-			//Elevator Variable
-			float elevFoward = ElevCont.GetRawAxis(3);
-			float elevBackward = ElevCont.GetRawAxis(2);
-
-			double netFoward = ((elevFoward - elevBackward));
-			double netBackward = ((elevBackward - elevFoward) * -1);
-
+			// Output Gear To SmartDashboard
+			SmartDashboard::PutNumber("Speed Gear", gear);
+			
+			/// Elevator
+			
+			// Gear Select
 			if(ElevCont.GetPOV() == 0) {
 				elevGear = 3;
 				elevSpeed = 1;
@@ -208,14 +196,20 @@ public:
 				elevGear = 1;
 				elevSpeed = 0.25;
 			}
-			//Elevator Control
 			
+			// Movement
+			elevFoward = ElevCont.GetRawAxis(3);
+			elevBackward = ElevCont.GetRawAxis(2);
+
+			netFoward = elevFoward - elevBackward;
+			netBackward = (elevBackward - elevFoward) * -1;
+
 			elevTT.Set(netFoward * elevSpeed);
 			elevTT.Set(netBackward * elevSpeed);
-				
-			//Solenoid Control
 			
-			//Large Cylinders
+			/** -------- Solenoid Control --------------- */
+			
+			// Large Cylinders
 			if (ElevCont.GetRawButton(5)){
 				largeC.Set(DoubleSolenoid::Value::kReverse);
 				Wait(1);
@@ -232,7 +226,7 @@ public:
 				largeC.Set(DoubleSolenoid::Value::kOff);
 			}
 
-			//Small Cylinders AKA Release Function
+			// Small Cylinders AKA Release Function
 			if (ElevCont.GetRawButton(3)){
 				releaseC.Set(DoubleSolenoid::Value::kReverse);
 				Wait(1);
@@ -248,9 +242,7 @@ public:
 			if (ElevCont.GetRawButton(1)){
 				releaseC.Set(DoubleSolenoid::Value::kOff);
 			}
-
 			
-
 			frc::Wait(0.005);
 		}
 	}
